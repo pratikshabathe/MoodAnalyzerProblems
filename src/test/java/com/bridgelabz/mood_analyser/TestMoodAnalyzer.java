ef950ff77b2f91c5d6f730add73ed11c452ab9f8
pratikshabathe <pratikshabathe11@gmail.com>
2023-02-10 19:20:26 +0530

    Added: Happy mood massage

--- src/test/java/com/bridgelabz/mood_analyser/TestMoodAnalyzer.java
+++ src/test/java/com/bridgelabz/mood_analyser/TestMoodAnalyzer.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 
 public class TestMoodAnalyzer {
 	@Test
-	public void testMessageContainSad() throws MoodAnalyzerException {
-		MoodAnalyzer moodAnalyser = new MoodAnalyzer("I am in sad mood");
-		String SadMood = moodAnalyser.analyseMood();
-		System.out.println(SadMood);
-		assertEquals("SAD" , SadMood);
+	public void testMessageContainHappy() throws MoodAnalyzerException {
+		MoodAnalyzer moodAnalyser = new MoodAnalyzer("I am in happy mood");
+		String HappyMood = moodAnalyser.analyseMood();
+		System.out.println(HappyMood);
+		assertEquals("HAPPY" , HappyMood);
 	}
 }
